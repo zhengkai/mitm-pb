04a47dc9d8d4f61864e3936059cd53c8e53071b5
Zheng Kai <zhengkai@gmail.com>
2021-04-09 18:18:38 +0800

    pb

--- server/src/demo.go
+++ server/src/demo.go
@@ -1,8 +1,19 @@
 package demo
 
-import "demo/web"
+import (
+	"demo/pb"
+	"demo/web"
+	"fmt"
+)
 
 // Start ...
 func Start() {
+
+	fmt.Println(&pb.Player{
+		ID:    1,
+		Level: 2,
+		Class: pb.Class_Mage,
+	})
+
 	web.Server(5678)
 }
